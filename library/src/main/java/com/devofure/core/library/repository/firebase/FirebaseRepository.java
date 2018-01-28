6f669f1c9e8f182bd6ece3e64fe5eac1866a2c6f
Erick <devofure@gmail.com>
2018-01-29 00:55:51 +0100

    added DbModel
    Took 11 minutes

--- library/src/main/java/com/devofure/core/library/repository/firebase/FirebaseRepository.java
+++ library/src/main/java/com/devofure/core/library/repository/firebase/FirebaseRepository.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 
 import com.devofure.core.library.repository.BaseRepository;
 import com.devofure.core.library.repository.ParserTask;
+import com.devofure.core.library.repository.model.DbModel;
 import com.google.firebase.database.DataSnapshot;
 
 public abstract class FirebaseRepository extends BaseRepository {
@@ -20,6 +21,26 @@ public abstract class FirebaseRepository extends BaseRepository {
 		return rawData -> {
 			if (rawData.exists()) {
 				return rawData.getValue(tClass);
+			}
+			return null;
+		};
+	}
+
+	/**
+	 * gives a default ParserTask with the key as internal key
+	 *
+	 * @param tClass
+	 * @param <T>
+	 * @return
+	 */
+	@NonNull
+	protected <T extends DbModel> ParserTask<T, DataSnapshot> geMappingWithKeyTask(Class<T> tClass) {
+		return rawData -> {
+			if (rawData.exists()) {
+				T t = rawData.getValue(tClass);
+				if (t != null) {
+					t.setKey(rawData.getKey());
+				}
 			}
 			return null;
 		};
---
+++ library/src/main/java/com/devofure/core/library/repository/model/DbModel.java
@@ -0,0 +1,19 @@
+package com.devofure.core.library.repository.model;
+
+/**
+ * Created by chavez on 2018-01-28.
+ */
+
+public abstract class DbModel {
+
+	String key;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+}
